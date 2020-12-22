768ad7cdfba90ab073ebd4e50c32450bf656aef2
mccleskeyc <mccleskeyc@gmail.com>
2020-12-22 12:16:58 -0500

    commit tree 66049de054602d442a6c899bcf290b86869fcad7

--- app.rb
+++ app.rb
@@ -1,16 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  get '/reverse' do
+  get '/reverse' do # renders the reverse.erb page
     erb :reverse
   end
 
-  post '/reverse' do
+  post '/reverse' do # receives the params hash from the form and renders the reversed.erb
+    original_string = params["string"]
+    @reversed_string = original_string.reverse
     erb :reversed
   end
 
-  get '/friends' do
+  get '/friends' do #renders the friends.erb page
     # Write your code here!
+    @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
 
+    erb :friends
   end
 end
